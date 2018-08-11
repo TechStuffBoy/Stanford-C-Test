feff54e457d3ce7f9ed255e83632987979e88aa4
Nandha_Kumar_M <nandhakumar@cpsgateway.com>
2018-08-11 12:42:13 +0530

    basic Struct Address

---
+++ Struct/src/BasicAddress.c
@@ -0,0 +1,32 @@
+/*
+ ============================================================================
+ Name        : Struct.c
+ Author      : Nandha Kumar M
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct  {
+	float num;
+	float denum;
+} fraction;
+
+fraction  pi,pi1;
+
+int main(void) {
+	printf("Address of pi=%p\n",&pi);
+	printf("Addres of pi.num=%p\n", &pi.num);
+	printf("Addres of pi.denum=%p\n", &pi.denum);
+
+	printf("Address of pi1=%p\n",&pi1);
+	printf("Addres of pi1.num=%p\n", &pi1.num);
+	printf("Addres of pi1.denum=%p\n", &pi1.denum);
+}
+*/
